147309bdf010580038b5ff27baa0e37662d64fb9
joaocorner <joaoeduardocorner@gmail.com>
2023-01-02 22:14:14 -0300

    refactor: changing css to styled components at main content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import styled, { css } from "styled-components";
 
 function App() {
-  return <div></div>;
+  const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid white;
+    color: white;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+
+    /* ${(props) =>
+      props.primary &&
+      css`
+        background: palevioletred;
+        color: white;
+      `}; */
+  `;
+  const Container = styled.div`
+    text-align: center;
+    background-color: #c4b3dd;
+    height: 100vh;
+  `;
+  return (
+    <Container>
+      <Button>Todos os Produtos</Button>
+      {/* <Button primary>Primary Button</Button> */}
+    </Container>
+  );
 }
 
 export default App;
