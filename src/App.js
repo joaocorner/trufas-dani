20fd7d24ed10a2b463ec02df5c6d0b005d0043ee
joaocorner <joaoeduardocorner@gmail.com>
2023-01-04 23:03:05 -0300

    layout: dividing first page into 2

--- src/App.js
+++ src/App.js
@@ -20,11 +20,21 @@ function App() {
     text-align: center;
     background-color: #c4b3dd;
     height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    img {
+      object-fit: cover;
+      height: 100vh;
+      width: 50vw;
+    }
   `;
   return (
     <Container>
       <Button>Todos os Produtos</Button>
       {/* <Button primary>Primary Button</Button> */}
+      <img src="../img/IMG_20221231_233735.jpg" alt="" />
     </Container>
   );
 }
